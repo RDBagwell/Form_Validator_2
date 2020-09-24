5fbafea2f06f6312034af5b909438bfcc031389a
Robert B <robertb@lifesciencepublishers.com>
2020-09-24 14:16:38 -0600

    Add JS

--- js/script.js
+++ js/script.js
@@ -14,23 +14,58 @@ function showError(element, message){
 
 function showSuccess(element){
     const formControl = element.parentElement;
-    const messageEL = formControl.querySelector('small');
     formControl.classList.add('success');
     formControl.classList.remove('error');
 }
 
-function validateEmail(email) {
+function validateEmail(input) {
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
+    if(re.test(input.value.trim())){
+        showSuccess(input)
+    } else {
+        showError(input, `${getFieldName(input)} is not valid.`);
+    }
 }
 
-function checkRequired(input) {
-    console.log(input)
+function checkRequired(inputArray) {
+    inputArray.forEach(input=>{
+        if(input.value.trim() === ''){
+            showError(input, `${getFieldName(input)} is required.`);
+        } else {
+            showSuccess(input);
+        }
+    });
+}
+
+function getFieldName(input) {
+    return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
+function checkPasswordMatch(input1, input2) {
+    if(input1.value === input2.value){
+        showSuccess(input1);
+        showSuccess(input2);
+    } else {
+        showError(input2, 'Passwords do not match.')
+    }
+}
+
+function checkLength(input, min, max) {
+    if(input.value.length < min){
+        showError(input, `${getFieldName(input)} must be at least ${min} characters`);
+    } else if(input.value.length > max){
+        showError(input, `${getFieldName(input)} can't be longer than ${max} characters`);
+    } else {
+        showSuccess(input);
+    }
 }
 
 // Event Listeners
 form.addEventListener('submit', (e)=>{
     e.preventDefault();
-    const input_feids = [username, email, password, password2]
+    const input_feids = [username, email, password, password2];
     checkRequired(input_feids);
+    validateEmail(email);
+    checkLength(password, 6, 30);
+    checkPasswordMatch(password, password2)
 });
